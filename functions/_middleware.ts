2a99bb73eece483b2114b6aceb01aa2108df62c8
Rastislav <rastislav@onion.email>
2024-05-31 22:17:08 +0200

    Logs

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -12,10 +12,12 @@ declare const KV_NAMESPACE: KVNamespace;
 // Static forms plugin
 const staticForms = createStaticFormsPlugin({
     respondWith: async ({ formData, name }: { formData: FormData, name: string }) => {
+        console.log(`Received form: ${name}`);
         switch (name) {
             case 'verify-person':
                 return checkRepresentative(formData);
             default:
+                console.error('Invalid form name');
                 return new Response('Invalid form name', { status: 400 });
         }
     },
@@ -27,6 +29,7 @@ const checkRepresentative = async (formData: FormData): Promise<Response> => {
     let username = formData.get('username')?.toString().trim().toLowerCase();
 
     if (!type || !username) {
+        console.error('Invalid form data');
         return new Response('Invalid form data', { status: 400 });
     }
 
@@ -35,11 +38,14 @@ const checkRepresentative = async (formData: FormData): Promise<Response> => {
 
     // Create the key
     const key = `${type}:${username}`;
+    console.log(`Looking up KV for key: ${key}`);
     const representative = await KV_NAMESPACE.get(key);
 
     if (representative) {
+        console.log(`User found: ${representative}`);
         return new Response(`User: '${representative}' is an official representative under this contact.`, { status: 200 });
     } else {
+        console.warn('User not found');
         return new Response("This contact is not authorized as an official contact!", { status: 404 });
     }
 }
@@ -57,6 +63,7 @@ export const onRequestPost: PagesFunction<Env> = async (context) => {
     // Run hcaptcha verification first
     const hcaptchaResponse = await hcaptchaConfig(context);
     if (hcaptchaResponse) {
+        console.error('hCaptcha verification failed');
         return hcaptchaResponse;
     }
 
