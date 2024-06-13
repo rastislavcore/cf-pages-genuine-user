3db2d9d000ddbbbf6795f096fb3692a9da6af192
Rastislav <rastislav@onion.email>
2024-06-13 14:44:36 +0200

    hcaptcha test

--- functions/api/1/verify.ts
+++ functions/api/1/verify.ts
@@ -11,7 +11,7 @@ export async function onRequestPost(context) {
     const { env, request } = context;
     try {
         // Update hcaptcha configuration with the environment variables
-        /*const hcaptchaConfig = hcaptchaVerify({
+        const hcaptchaConfig = hcaptchaVerify({
             secret: env.HCAPTCHA_SECRET,
             sitekey: env.HCAPTCHA_SITE_KEY,
         });
@@ -26,27 +26,28 @@ export async function onRequestPost(context) {
                 status: 400,
                 headers: { 'Content-Type': 'application/json' },
             });
-        }*/
+        }
 
-        console.log('Handling POST request');
         const contentType = request.headers.get('content-type');
-        console.log('Content-Type:', contentType);
-
         if (!contentType || !contentType.includes('multipart/form-data')) {
-            console.log('Unsupported Media Type');
-            return new Response('Unsupported Media Type', { status: 415 });
+            return new Response(JSON.stringify({
+                message: `Unsupported Media Type.`,
+            }), {
+                status: 415,
+                headers: { 'Content-Type': 'application/json' },
+            });
         }
 
         const formData = await request.formData();
-        const type = formData.get('type');
-        const username = formData.get('username');
-
-        console.log('Form Data:', { type, username });
 
         return await checkRepresentative(formData, env.AUTHORIZED_CONTACTS);
     } catch (error) {
-        console.error('Error processing form:', (error as Error).message);
-        return new Response(`Error processing form: ${(error as Error).message}`, { status: 500 });
+        return new Response(JSON.stringify({
+            message: `Error processing the form.`,
+        }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
     }
 }
 
@@ -55,9 +56,8 @@ const checkRepresentative = async (formData: FormData, kvNamespace: KVNamespace)
     let username = formData.get('username')?.toString().trim().toLowerCase();
 
     if (!type || !username) {
-        console.error('Invalid form data');
         return new Response(JSON.stringify({
-            message: `Invalid form data`,
+            message: `Invalid form data.`,
         }), {
             status: 400,
             headers: { 'Content-Type': 'application/json' },
@@ -69,12 +69,10 @@ const checkRepresentative = async (formData: FormData, kvNamespace: KVNamespace)
 
     // Create the key
     const key = `${type}:${username}`;
-    console.log(`Looking up KV for key: ${key}`);
     const representative = await kvNamespace.get(key);
 
     try {
         if (representative) {
-            console.log(`User found: ${representative}`);
             return new Response(JSON.stringify({
                 message: `User: '${representative}' is an official representative under this contact.`,
             }), {
@@ -82,7 +80,6 @@ const checkRepresentative = async (formData: FormData, kvNamespace: KVNamespace)
                 headers: { 'Content-Type': 'application/json' },
             });
         } else {
-            console.warn('User not found');
             return new Response(JSON.stringify({
                 message: `This contact is not authorized as an official contact!`,
             }), {
@@ -91,7 +88,11 @@ const checkRepresentative = async (formData: FormData, kvNamespace: KVNamespace)
             });
         }
     } catch (error) {
-        console.error('Error processing form:', (error as Error).message);
-        return new Response(`Error processing form: ${(error as Error).message}`, { status: 500 });
+        return new Response(JSON.stringify({
+            message: `Error searching for the data.`,
+        }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
     }
 }
